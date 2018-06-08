9e14d2574b9a1a622d80c987ecf071d777373591
龙见 <kaslo@163.com>
2018-06-08 23:57:41 +0800

    FIX 增加rgb颜色生成类

---
+++ src/main/java/info/ideatower/magic/random/color/RgbColor.java
@@ -0,0 +1,23 @@
+package info.ideatower.magic.random.color;
+
+import info.ideatower.magic.random.AbstractRandomValue;
+import info.ideatower.magic.random.value.Int;
+import info.ideatower.magic.util.Repeater;
+
+import java.text.MessageFormat;
+
+public class RgbColor extends AbstractRandomValue<String> {
+
+    private Int integer;
+
+    public RgbColor(String mark) {
+        super(mark);
+        // 因为Int使用的是java.lang.Random类对象的next(int bounds) 该方法会排除掉传入的max值，所以需要+1
+        this.integer = new Int(mark, 0, 255 + 1);
+    }
+
+    @Override
+    public String next() {
+        return MessageFormat.format("rgb({0}, {1}, {2})", Repeater.repeat(this.integer, 3));
+    }
+}
