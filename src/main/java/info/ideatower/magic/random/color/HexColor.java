ca7ed496f449b71b47217ffced888d12ccc3f427
龙见 <kaslo@163.com>
2018-06-08 23:57:57 +0800

    FIX 修复hex颜色生成类

--- src/main/java/info/ideatower/magic/random/color/HexColor.java
+++ src/main/java/info/ideatower/magic/random/color/HexColor.java
@@ -1,12 +1,25 @@
 package info.ideatower.magic.random.color;
 
 import info.ideatower.magic.random.AbstractRandomValue;
+import info.ideatower.magic.random.text.Letter;
+import info.ideatower.magic.random.text.Str;
+import info.ideatower.magic.random.value.Picker;
+import info.ideatower.magic.util.Repeater;
+
+import java.text.MessageFormat;
 
 public class HexColor extends AbstractRandomValue<String> {
 
 
+    private Letter letter;
+
+    public HexColor(String mark) {
+        super(mark);
+        this.letter = new Letter(mark, "0123456789ABCDEF");
+    }
+
     @Override
     public String next() {
-        return null;
+        return MessageFormat.format("#{0}{1}{2}{3}{4}{5}", Repeater.repeat(letter, 6));
     }
 }
