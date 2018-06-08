a4c188fe6996c8ae479389c6f9fa3d2a55560a30
龙见 <kaslo@163.com>
2018-06-09 00:11:38 +0800

    FIX 完善 Gender 性别

--- src/main/java/info/ideatower/magic/random/person/Gender.java
+++ src/main/java/info/ideatower/magic/random/person/Gender.java
@@ -3,8 +3,16 @@ package info.ideatower.magic.random.person;
 import com.google.common.collect.Lists;
 import info.ideatower.magic.random.value.Picker;
 
+/**
+ * 性别
+ */
 public class Gender extends Picker<String> {
+
     public Gender(String mark) {
-        super(mark, Lists.newArrayList("FEMALE", "MALE"));
+        super(mark, Lists.newArrayList("女", "男"));
+    }
+
+    public Gender(String mark, String female, String male) {
+        super(mark, Lists.newArrayList(female, male));
     }
 }
