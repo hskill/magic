cf0b41ee28b02e8bd56006b5ead1228533dab2d4
龙见 <kaslo@163.com>
2018-06-09 00:12:20 +0800

    FIX 完善 Str 年龄

--- src/main/java/info/ideatower/magic/random/text/Str.java
+++ src/main/java/info/ideatower/magic/random/text/Str.java
@@ -3,16 +3,38 @@ package info.ideatower.magic.random.text;
 import info.ideatower.magic.random.AbstractRandomValue;
 import org.apache.commons.lang3.RandomStringUtils;
 
+/**
+ * 生成随机字符串，可以指定长度，或长度范围，以及指定字符池
+ */
 public class Str extends AbstractRandomValue<String> {
 
     public static final String STRS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    private String pool = STRS;
     private int count;
     private int min;
     private int max;
 
     public Str(String mark) {
+        this(mark, STRS, 5);
+    }
+
+    public Str(String mark, String pool, int count) {
+        super(mark);
+        this.pool = pool;
+        this.count = count;
+    }
+
+    public Str(String mark, String pool, int min, int max) {
+        super(mark);
+        this.pool = pool;
+        this.min = min;
+        this.max = max;
+    }
+
+    public Str(String mark, String pool) {
         this(mark, 5);
+        this.pool = pool;
     }
 
     public Str(String mark, int count) {
