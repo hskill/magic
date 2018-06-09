b718fe5f4cf22e7dfe2ebcc12b6c8a4a2d719d95
龙见 <kaslo@163.com>
2018-06-09 13:10:05 +0800

    FIX 完善 Image

--- src/main/java/info/ideatower/magic/random/web/Image.java
+++ src/main/java/info/ideatower/magic/random/web/Image.java
@@ -1,6 +1,8 @@
 package info.ideatower.magic.random.web;
 
 import info.ideatower.magic.random.AbstractRandomValue;
+import info.ideatower.magic.random.color.HexColor;
+import info.ideatower.magic.random.text.Letter;
 
 import java.text.MessageFormat;
 
@@ -11,12 +13,88 @@ import java.text.MessageFormat;
  */
 public class Image extends AbstractRandomValue<String> {
 
-    public Image(String mark) {
+    public static final int JPG = 1;
+    public static final int PNG = 2;
+    public static final int GIF = 3;
 
+    private final HexColor color;
+    private final Letter letter;
+
+    private int width = Integer.MIN_VALUE;
+    private int height = Integer.MIN_VALUE;
+    private int format = 1; // 1. jpg 2. png 3. gif
+    private String ratio;
+
+    public Image(String mark) {
+        super(mark);
+        this.color = new HexColor(mark);
+        this.letter = new Letter(mark, true);
+    }
+
+    public Image width(int width) {
+        this.width = width;
+        return this;
+    }
+
+    public Image height(int height) {
+        this.height = height;
+        return this;
+    }
+
+    public Image format(int format) {
+        this.format = format;
+        return this;
+    }
+
+    public Image ratio(int widthRatio, int heightRatio) {
+        this.ratio = widthRatio + ":" + heightRatio;
+        return this;
     }
 
     @Override
     public String next() {
-        return MessageFormat.format("https://dummyimage.com/{0}x{1}/{2}/009922.{3}&text={4}", "");
+        return MessageFormat.format("https://dummyimage.com/{0}/{1}/{2}.{3}&text={4}",
+                getSize(),
+                this.color.next().substring(1),
+                this.color.next().substring(1),
+                getFormatStr(),
+                this.letter.next());
+    }
+
+    private String getFormatStr() {
+        if (this.format == PNG) {
+            return "png";
+        }
+        else if (this.format == GIF) {
+            return "gif";
+        }
+        else {
+            return "jpg";
+        }
+    }
+
+    public String getSize() {
+        if (this.height == Integer.MIN_VALUE && this.width != Integer.MIN_VALUE) {
+
+            if (this.ratio != null) {
+                return this.width + "x" + this.ratio;
+            }
+
+            return String.valueOf(this.width);
+        }
+        else if (this.height != Integer.MIN_VALUE && this.width == Integer.MIN_VALUE) {
+
+            if (this.ratio != null) {
+                return this.ratio + "x" + this.height;
+            }
+            return String.valueOf(this.height);
+        }
+        else if (this.width == Integer.MIN_VALUE && this.height == Integer.MIN_VALUE) {
+            // default value
+            return "200";
+        }
+        else {
+            return this.width + "x" + this.height;
+        }
     }
 }
